3981e6109a79bb8ea4f1b433eec95ad9c5d566b6
Daniel Worrall <Daniel.Worrall@hotmail.co.uk>
2016-12-26 21:25:32 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+gem 'discordrb', git: 'https://github.com/meew0/discordrb.git', branch: 'gateway-reactions'
+gem 'rubocop', '~> 0.46.0', require: false
---
+++ discop.rb
@@ -0,0 +1,56 @@
+require 'discordrb'
+require 'yaml'
+require 'rubocop'
+
+CHANNEL_ID = 83_281_822_225_530_880.freeze
+RUBOCOP_EMOTE = 'rubocop%3A246708457460334592'.freeze
+PASS_EMOTE = 'helYea%3A236243426662678528'.freeze
+FAIL_EMOTE = 'helNa%3A239120424938504192'.freeze
+DELETE_EMOTE = '❌'.freeze
+
+@secrets = YAML.load_file('secret.yml')
+bot = Discordrb::Bot.new token: @secrets[:token]
+
+def rubocop_this(team, text)
+  processed = RuboCop::ProcessedSource.new(text, 2.4)
+  offenses = team.inspect_file(processed)
+  offenses.collect do |o|
+    l = o.location
+    c = o.severity.code
+    line = l.line == l.last_line
+    "**L#{l.line}:#{l.column + 1}: #{c}:** *#{o.message}*\n```ruby\n#{l.source_line}#{' ...' unless line}\n#{' ' * l.column + '^' * (line ? l.last_column - l.column : l.source_line.length - l.column)}```"
+  end.join("\n")
+end
+
+bot.message(in: CHANNEL_ID) do |event|
+  next unless event.content =~ /```(?:ruby|rb)\n[\s\S]+```/i
+  event.message.react(RUBOCOP_EMOTE)
+  reply = []
+  event.content.scan(/```(?:ruby|rb)\n([\s\S]+?)```/i).each do |ruby_code|
+    cops = RuboCop::Cop::Cop.non_rails - [RuboCop::Cop::Style::FileName, RuboCop::Cop::Style::TrailingBlankLines]
+    config = RuboCop::ConfigLoader.default_configuration
+    team = RuboCop::Cop::Team.new(cops, config)
+    message = rubocop_this(team, ruby_code[0])
+    unless message.empty?
+      event.message.react(FAIL_EMOTE) if reply.empty?
+      reply << message
+    end
+  end
+  if reply.empty?
+    event.message.react(PASS_EMOTE)
+  else
+    message = event.channel.send_embed(event.user.mention) do |embed|
+      embed.author = Discordrb::Webhooks::EmbedAuthor.new(name: 'RuboCop', url: 'https://github.com/bbatsov/rubocop', icon_url: 'https://gratipay.com/rubocop/image')
+      embed.description = reply.join('')
+    end
+    message.react(DELETE_EMOTE)
+  end
+  nil
+end
+
+bot.reaction_add(emoji: DELETE_EMOTE) do |event|
+  event.message.delete if event.channel.id == CHANNEL_ID && event.message.mentions.first == event.user && event.message.user == bot.profile
+  nil
+end
+
+bot.run
